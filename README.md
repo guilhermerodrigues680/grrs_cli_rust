ed3f11d82eea778c4ff272f3792b03f0924d2cc4
guilhermerodrigues680 <guilhermerodrigues680@gmail.com>
2023-02-05 17:56:51 -0300

    Parse manual dos argumentos.

---
+++ README.md
@@ -0,0 +1,10 @@
+[https://rust-cli.github.io/book/index.html]
+
+$ grrs foobar test.txt
+
+Program to look at test.txt and print out the lines that contain foobar.
+
+
+$ cargo run --example exname -- --exoption exarg1 exarg2
+
+$ cargo run -- foobar test.txt
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
+use std::str::FromStr;
+
+#[derive(Debug)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
+fn get_args() -> Result<Cli, Box<dyn std::error::Error>> {
+    let pattern = std::env::args().nth(1).ok_or_else(|| "no pattern given")?;
+    let path = std::env::args().nth(2).ok_or_else(|| "no path given")?;
+    let path = std::path::PathBuf::from_str(&path)?;
+    Ok(Cli { pattern, path })
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let cli = get_args()?;
+
+    dbg!(cli);
     println!("Hello, world!");
+
+    Ok(())
 }
